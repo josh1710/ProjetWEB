567fad40aa7fee5c68fbdb35117fc2f58911d4c3
josh1710 <joshuaribeiro17@gmail.com>
2024-05-03 09:48:44 +0200

    fichier js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Au clic sur le bouton Rechercher, afficher une alerte avec le contenu du champ de recherche
+let button = document.querySelector("button");
+
+button.addEventListener("click", function() {
+  let input = document.querySelector("input[type='text']");
+  alert("Vous avez cherché : " + input.value);
+});
